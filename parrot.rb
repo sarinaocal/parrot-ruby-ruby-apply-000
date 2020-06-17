bc4a78db54516c18bd87b57c66ad261e5eeffae5
Sarina Ocal <sarinaocal@gmail.com>
2020-06-17 14:50:19 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,18 +1,2 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase = "Pretty Bird!")
-
-puts "Squawk!" 
-
-return "Squawk!"
-  
-end
-
-def parrot(phrase)
-
-puts phrase 
-
-return phrase
-
-
-end
